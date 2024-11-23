71445c7be5764d6dffec3fa5072754468b49a28b
nashaofu <nashaofu@users.noreply.github.com>
2024-11-23 22:19:37 +0800

    feat: 编写测试例子

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useCallback, useState } from 'react'
+import './App.css'
+import { save, open } from '@tauri-apps/plugin-dialog'
+import { writeFile, readFile } from '@tauri-apps/plugin-fs'
+
+function App() {
+  const onSave = useCallback(async () => {
+    const path = await save()
+    if (!path) {
+      return
+    }
+
+    let encoder = new TextEncoder()
+    let data = encoder.encode('Hello World')
+    await writeFile(path, data)
+  }, [])
+  
+  const onOpen = useCallback(async () => {
+    const path = await open()
+    if (!path) {
+      return
+    }
+
+    let content = await readFile(path)
+    console.log(content)
+  }, [])
+  
+  return (
+    <main className="container">
+      <button onClick={onSave}>save file</button>
+      <button onClick={onOpen}>open file</button>
+    </main>
+  )
+}
+
+export default App
